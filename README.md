94094ee03463986595d50e5409a17f300f553271
Scott Ha <123viet123@gmail.com>
2020-11-19 21:01:30 -0800

    Changed it up

---
+++ README.md
@@ -0,0 +1,9 @@
+# ChessBot
+A simple DIscord Bot for me to mess around with
+
+## Dependancies
+- Winston
+- Discord.io
+
+## Running The Bot
+```npm start```
--- bot.js
+++ bot.js
@@ -1,39 +1,20 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+// require the discord.js module
+const Discord = require('discord.js');
+const { token } = require('./auth.json');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to.
-         }
-     }
+
+// login to Discord with your app's token
+client.login(token);
+
+// listen for messages
+client.on('message', message => {
+  console.log(message);
 });
